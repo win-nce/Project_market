9a0a551af6b9dca66ec24e980ce525fe7c4387c1
win-nce <jumikirali@gmail.com>
2025-04-28 01:30:11 +0500

    Update product4.js

--- product4.js
+++ product4.js
@@ -1,3 +1,45 @@
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        const target = link.getAttribute('data-target');
+        switch(target) {
+            case 'home':
+                window.location.href = 'home.html';
+                break;
+            case 'cart':
+                window.location.href = 'cart.html';
+                break;
+            case 'login':
+                window.location.href = 'login.html';
+                break;
+            case 'contacts':
+                if (window.location.pathname.includes('home.html')) {
+                    document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
+                } else {
+                    window.location.href = 'home.html#footer';
+                }
+                break;
+        }
+    });
+});
+    const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    effect: 'fade',
+    speed: 800, 
+    });
+    
 document.querySelector('.add-to-cart').addEventListener('click', function() {
     const product4 = {
         name: "Spiral zip pants -KHAKI-",
